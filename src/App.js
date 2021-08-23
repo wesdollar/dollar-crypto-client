2ef82e120f0aa843e1e48ee22f2e9610892cc135
Wes Dollar <wes.dollar@progleasing.com>
2021-08-23 13:53:39 -0500

    preparing for deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Dashboard } from "@wesdollar/dollar-ui.views.dashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isFetching, setIsFetching] = useState(true);
+  const [profits, setProfits] = useState({});
+
+  useEffect(() => {
+    const callProfits = async () => {
+      const response = await fetch(process.env.REACT_APP_API_URL);
+      const json = await response.json();
+
+      if (Object.keys(json).length) {
+        setProfits(json);
+        setIsFetching(false);
+      }
+    };
+
+    callProfits();
+  }, []);
+
+  return <Dashboard isLoading={isFetching} profitsResource={profits} />;
 }
 
 export default App;
