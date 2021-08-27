3009d16c9af1764c3690a331d3aed2b94ef95bb7
Wes Dollar <wes.dollar@progleasing.com>
2021-08-27 00:23:26 -0500

    adding set keys

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Github } from "@wesdollar/dollar-ui.auth.github";
 import { LogoPngMd } from "@wesdollar/dollar-crypto.logo.logo-png-md";
 import { Space } from "@wesdollar/dollar-ui.ui.space";
 import { Button } from "@wesdollar/dollar-crypto.dollar-crypto.ui.buttons.button";
+import { SetKeys } from "@wesdollar/dollar-crypto.dollar-crypto.views.set-keys";
 
 const {
   REACT_APP_GOOGLE_API_KEY,
@@ -15,6 +16,8 @@ const {
   REACT_APP_GOOGLE_APP_ID,
   REACT_APP_GOOGLE_MEASUREMENT_ID,
 } = process.env;
+
+const { REACT_APP_API_URL: apiUrl } = process.env;
 
 const Container = styled.div`
   display: grid;
@@ -40,24 +43,53 @@ const FlexContainer = styled.div`
 `;
 
 function App() {
+  /* eslint-disable no-unused-vars */
   const [isFetching, setIsFetching] = useState(true);
   const [profits, setProfits] = useState({});
   const [authenticatedUser, setAuthenticatedUser] = useState({});
   const [userIsAuthenticated, setUserIsAuthenticated] = useState(false);
   const [authErrors, setAuthErrors] = useState();
   const [authResult, setAuthResult] = useState();
+  const [userDetails, setUserDetails] = useState({});
+  const [displaySetCreds, setDisplaySetCreds] = useState(false);
+  const [validationErrors, setValidationErrors] = useState({});
+  /* eslint-enable */
 
-  useEffect(() => console.error("auth errors", authErrors), [authErrors]);
-  useEffect(() => console.log("auth result", authResult), [authResult]);
   useEffect(
-    () => console.log("auth user", authenticatedUser),
+    () =>
+      process.env.REACT_APP_DEBUG &&
+      console.log("auth user", authenticatedUser),
     [authenticatedUser]
   );
 
   useEffect(() => {
+    setIsFetching(true);
+
+    const callGetUserDetails = async () => {
+      const response = await fetch(
+        `${apiUrl}/users?token=${authenticatedUser}`
+      );
+      const json = await response.json();
+
+      if (Object.keys(json).length) {
+        setUserDetails(json);
+
+        if (!json.cbKey || !json.cbSecret) {
+          setDisplaySetCreds(true);
+          setIsFetching(false);
+        }
+      }
+    };
+
+    if (userIsAuthenticated) {
+      callGetUserDetails();
+    }
+  }, [userIsAuthenticated, authenticatedUser, displaySetCreds]);
+
+  useEffect(() => {
     const callProfits = async () => {
       const response = await fetch(
-        `${process.env.REACT_APP_API_URL}?token=${authenticatedUser}`
+        `${apiUrl}/profits?token=${authenticatedUser}`
       );
       const json = await response.json();
 
@@ -67,10 +99,47 @@ function App() {
       }
     };
 
-    if (userIsAuthenticated) {
+    if (
+      Object.keys(userDetails).length &&
+      userDetails.cbKey &&
+      userDetails.cbSecret &&
+      userIsAuthenticated
+    ) {
       callProfits();
     }
-  }, [userIsAuthenticated, authenticatedUser]);
+  }, [userDetails, authenticatedUser, userIsAuthenticated]);
+
+  const handleSaveKeys = async () => {
+    const cbKey = document.querySelector("#key").value;
+    const cbSecret = document.querySelector("#secret").value;
+
+    const response = await fetch(`${apiUrl}/set-keys`, {
+      method: "POST",
+      body: JSON.stringify({
+        cbKey,
+        cbSecret,
+        token: authenticatedUser,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status === 200) {
+      setDisplaySetCreds(false);
+      setIsFetching(true);
+    } else {
+      setValidationErrors(response.error);
+    }
+  };
+
+  if (displaySetCreds) {
+    return (
+      <FlexContainer>
+        <SetKeys onSave={handleSaveKeys} />
+      </FlexContainer>
+    );
+  }
 
   if (!userIsAuthenticated) {
     return (
