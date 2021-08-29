c5ede1dedc4280bc1a99770dd66b750c523b9766
Wes Dollar <wes.dollar@progleasing.com>
2021-08-28 22:20:04 -0500

    version bump, adding bugsnag

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+
+const isProd = process.env.REACT_APP_ENV === "prod";
+let ErrorBoundary;
+
+if (isProd) {
+  Bugsnag.start({
+    apiKey: "ba990d2b1028f02e671c5f9dd3ad8f38",
+    plugins: [new BugsnagPluginReact()],
+  });
+
+  ErrorBoundary = Bugsnag.getPlugin("react").createErrorBoundary(React);
+} else {
+  ErrorBoundary = ({ children }) => children;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
